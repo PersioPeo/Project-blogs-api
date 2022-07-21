575b6aa3e82e1fbd7b9d5ec4e5ea61f8d52eca03
persio <persiopeo@hotmail.com>
2022-07-21 19:55:13 -0300

    feat: Criando o user do requisito-02

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,23 +1,25 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  User.init({
-    fullName: DataTypes.STRING
+const createUser = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+
   }, {
-    sequelize,
-    modelName: 'User',
+    tableName: 'Users',
+    timestamps: false,
   });
+  User.associate = (models) => { 
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'userId',      
+    })
+  }
   return User;
-};
+ };
+  
+module.exports = createUser;
