eedaabbd6776d3e25c881e120e8f44ef7440c4d2
persio <persiopeo@hotmail.com>
2022-07-28 20:47:19 -0300

    feat: criando o crud do login services

---
+++ src/services/loginService.js
@@ -0,0 +1,31 @@
+const { User } = require('../database/models');
+
+const criarService = async (displyName, email, password, image) => { 
+    const user = await User.findOrCreate({
+        where: { email },
+        defaults: { displyName, password, image },
+    });
+    return user;
+};
+
+const listarService = async () => { 
+    const users = await User.findAll({ attributes: { exclude: ['password'] } });
+    return users;
+};
+
+const pegarPorIdService = async (id) => {
+    const user = await User.findByPk(id, { attributes: { exclude: ['password'] } });
+    return user;
+};
+
+const removerService = async (id) => {
+    const user = await User.destroy({ where: { id } });
+    return user;
+};
+
+module.exports = {
+    criarService,
+    listarService,
+    pegarPorIdService,
+    removerService,
+};
