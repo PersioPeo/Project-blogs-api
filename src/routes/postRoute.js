f33149e7d379301f18eb1ce97674275803cd2be9
persio <persiopeo@hotmail.com>
2022-07-31 13:18:46 -0300

    feat: requisito-13

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -4,10 +4,11 @@ const rescue = require('express-rescue');
 
 const { authMiddleware } = require('../middleware');
 
-const { criarController } = require('../controllers/postController');
+const { criarController, listarController } = require('../controllers/postController');
 
 const postRouter = express.Router();
 
 postRouter.post('/post', authMiddleware, rescue(criarController));
+postRouter.get('/post', authMiddleware, rescue(listarController));
 
 module.exports = { postRouter };
